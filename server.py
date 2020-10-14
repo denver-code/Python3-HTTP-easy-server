63e00f316995bd578be367203b2e4b188c0802c2
denver-code <csigorek@gmail.com>
2020-10-14 22:20:52 +0300

    Add Try block

--- server.py
+++ server.py
@@ -5,6 +5,10 @@ PORT = 8000
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+try:
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+except:
+    print("\nHave some errors!")
+    httpd.server_close()
